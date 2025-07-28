ab1f8421c22c297a780e7cbaa9b110b4b5e0ac7c
Steve Moore <moore.stephen.m@wustl.edu>
2025-07-28 08:54:43 -0500

    Modify arguments for scripts/list_subjects_sessions.sh + Make the active projects file a required argument + Add optional argument: output file

--- scripts/list_subjects_sessions.sh
+++ scripts/list_subjects_sessions.sh
@@ -18,14 +18,15 @@ list_subjects_sessions() {
 #               authentication string (user or user:password)
 #               system (see common.sh)
 
-if [ $# -lt 2 ]; then
-    echo "Arguments: auth_string system [active projects file]"
+if [ $# -lt 3 ]; then
+    echo "Arguments: auth_string system active_projects_file [output file]"
     exit 1
 fi
 
 auth_string="$1"
 system="$2"
-shift 2
+active_projects="$3"
+shift 3
 
 BASE_FOLDER=$(dirname "$0")
 source "$BASE_FOLDER/common.sh"
@@ -35,11 +36,11 @@ set +e
 
 BOILER_PLATE=" -a $auth_string -x $url -e False "
 BOILER_PLATE=" -a $auth_string -x $url "
-ACTIVE_PROJECTS="test_data/active_projects.txt"
+OUTPUT_FILE="test_data/subjects_sessions.txt"
 if [[ $# -gt 0 ]] ; then
- ACTIVE_PROJECTS="$1"
+ OUTPUT_FILE="$1"
 fi
 
 # Run the prearchive code listing
-list_subjects_sessions "$BASE_FOLDER" "$BOILER_PLATE --verbose --csv $ACTIVE_PROJECTS " test_data/subjects_sessions.txt
+list_subjects_sessions "$BASE_FOLDER" "$BOILER_PLATE --verbose --csv $active_projects " $OUTPUT_FILE
 
