54ac9f86f1f7b5b4ccfe1556f81fbc603564de85
Steve Moore <moore.stephen.m@wustl.edu>
2025-07-29 15:59:43 -0500

    Modify processSessionXML.sh for efficiency and readability + Break up the work into a few different modules + Replace multiple grep calls over the same file to one larger grep using -f <file> switch

--- scripts/processSessionXML.sh
+++ scripts/processSessionXML.sh
@@ -3,116 +3,129 @@
 # CNDA to CNDA2 Migration Script Jenny Gurney, 6/6/2024
 
 
-INDIR=$1
-OUTDIR=$2
+BASE_INDIR=$1
+BASE_OUTDIR=$2
 REMOVESHARES=$3
 
-#REMOVELIST=$3
-#REPLACELIST=$4
+rm -rf   "${BASE_OUTDIR}"
+mkdir -p "${BASE_OUTDIR}"
 
-if [ -z "${INDIR}" ] || [ -z "${OUTDIR}" ] || [ ! -d "${INDIR}" ] || [ ! -d "${OUTDIR}" ]; then
+if [ -z "${BASE_INDIR}" ] || [ -z "${BASE_OUTDIR}" ] || [ ! -d "${BASE_INDIR}" ] || [ ! -d "${BASE_OUTDIR}" ]; then
    echo "Usage: processSessionXML.sh InDirectory OutDirectory ListOfProjectSharesToBeRemoved"
    exit 1
 fi
 
-for file in ${INDIR}/*.xml
-do
-   
-   # Create copy of XML in outdir
-   echo $file
-   basename $file
-   justFile=`basename $file` 
-   newFile=${OUTDIR}/$justFile
-   cp -f $file ${OUTDIR} 
+for project_folder in $BASE_INDIR/* ; do
+  echo $project_folder
+  INDIR=$project_folder
+  project_name=`basename $INDIR`
+  OUTDIR=$BASE_OUTDIR/$project_name
 
-   # Remove lines with strings found in remove list 
-   #while read removeStr 
-   #do
-   #   echo ${removeStr}
-   #   let i=0
-   #   for line in `cat $newFile | grep -n "${removeStr}" | cut -d: -f1`
-   #   do
-   #      echo ${line}
-   #      if [ ! -z "$line" ]; then
-   #         let changeLine=${line}-${i}
-   #         echo "sed -i \"${changeLine}d\" $newFile"
-   #         sed -i "${changeLine}d" $newFile
-   #         let i=$i+1
-   #      fi
-   #   done
-   #done < ${REMOVELIST}
+  echo $INDIR
+  echo $OUTDIR
+  mkdir -p $OUTDIR
 
-   # Remove shares with no share project listed -- these are broken share links
-   let i=0
-   for line in `cat $newFile | grep -n "<xnat:share" | grep -v project | cut -d: -f1`
-   do
-      let changeLine=${line}-${i}
-#      echo sed \"${changeLine}d\" $newFile
-           sed  "${changeLine}d"  $newFile > /tmp/sed.$$.txt
-#      echo mv /tmp/sed.$$.txt $newFile
-           mv /tmp/sed.$$.txt $newFile
-      let i=$i+1
-   done
+  for file in ${INDIR}/*.xml 
+  do
+     
+     # Create copy of XML in outdir
+     echo $file
+     basename $file
+     justFile=`basename $file` 
+     newFile=${OUTDIR}/$justFile
+     echo cp -f $file ${OUTDIR} 
+          cp -f $file ${OUTDIR} 
 
-   while read shareProj
-   do
-      shareStartLine=`cat $newFile | grep -n project=\"${shareProj}\" | cut -d: -f1`
-      if [ ! -z "$shareStartLine" ]; then
-#         echo sed \"${shareStartLine}d\" $newFile
-              sed "${shareStartLine}d" $newFile > /tmp/sed.$$.txt
-#         echo mv /tmp/sed.$$.txt $newFile
-              mv /tmp/sed.$$.txt $newFile
-      fi
-   done < ${REMOVESHARES}
 
-   # Remove xml share closure -- XNAT doesn't like it formatted this way and won't upload it
-   let i=0
-   for line in `cat $newFile | grep -n "</xnat:share>" | cut -d: -f1`
-   do
-        let changeLine=${line}-${i}
-#        echo sed \"${changeLine}d\" $newFile
-             sed  "${changeLine}d"  $newFile > /tmp/sed.$$.txt
-#        echo mv /tmp/sed.$$.txt $newFile
-             mv /tmp/sed.$$.txt $newFile
-        let i=$i+1
-   done
-
-   # Add inline closure to any remaining shares
-   for line in `cat $newFile | grep -n "<xnat:share" | cut -d: -f1`
-   do
-#      echo "sed \"${line}s/>/\/>/\" $newFile"
-           sed "${line}s/>/\/>/" $newFile > /tmp/sed.$$.txt
-      mv /tmp/sed.$$.txt $newFile
-   done   
-
-   # If there are no more shares in the XML, we need to remove "sharing"
-   areShares=`cat $newFile | grep -n "<xnat:share" | cut -d: -f1`
-   if [ -z "${areShares}" ]; then
-     let i=0 
-     for line in `cat $newFile | grep -n "xnat:sharing" | cut -d: -f1`
+  
+     # Remove lines with strings found in remove list 
+     #while read removeStr 
+     #do
+     #   echo ${removeStr}
+     #   let i=0
+     #   for line in `cat $newFile | grep -n "${removeStr}" | cut -d: -f1`
+     #   do
+     #      echo ${line}
+     #      if [ ! -z "$line" ]; then
+     #         let changeLine=${line}-${i}
+     #         echo "sed -i \"${changeLine}d\" $newFile"
+     #         sed -i "${changeLine}d" $newFile
+     #         let i=$i+1
+     #      fi
+     #   done
+     #done < ${REMOVELIST}
+  
+     # Remove shares with no share project listed -- these are broken share links
+     let i=0
+     for line in `cat $newFile | grep -n "<xnat:share" | grep -v project | cut -d: -f1`
      do
         let changeLine=${line}-${i}
-        echo "sed \"${changeLine}d\" $newFile"
-              sed  "${changeLine}d" $newFile > /tmp/sed.$$.txt
-        mv /tmp/sed.$$.txt $newFile
+  #      echo sed \"${changeLine}d\" $newFile
+             sed  "${changeLine}d"  $newFile > /tmp/sed.$$.txt
+  #      echo mv /tmp/sed.$$.txt $newFile
+             mv /tmp/sed.$$.txt $newFile
         let i=$i+1
      done
-   fi
-
-   # Find/replace strings from replace list 
-   #while read findReplaceStr
-   #do
-   #findStr=`echo $findReplaceStr | cut -d',' -f1`
-   #replaceStr=`echo $findReplaceStr | cut -d',' -f2`
-   #echo "find\/replace $findStr $replaceStr"
-   #if [ ! -z "${findStr}" ]; then
-   #   for line in `cat $newFile | grep -n "${findStr}" | cut -d: -f1`
-   #   do
-   #      echo "sed -i \"${line}s/${findStr}/${replaceStr}/g\" $newFile"
-   #      sed -i "${line}s/${findStr}/${replaceStr}/g" $newFile
-   #   done
-   #fi
-   #done < ${REPLACELIST}   
- 
-done 
-
+  
+     while read shareProj
+     do
+        shareStartLine=`cat $newFile | grep -n project=\"${shareProj}\" | cut -d: -f1`
+        if [ ! -z "$shareStartLine" ]; then
+  #         echo sed \"${shareStartLine}d\" $newFile
+                sed "${shareStartLine}d" $newFile > /tmp/sed.$$.txt
+  #         echo mv /tmp/sed.$$.txt $newFile
+                mv /tmp/sed.$$.txt $newFile
+        fi
+     done < ${REMOVESHARES}
+  
+     # Remove xml share closure -- XNAT doesn't like it formatted this way and won't upload it
+     let i=0
+     for line in `cat $newFile | grep -n "</xnat:share>" | cut -d: -f1`
+     do
+          let changeLine=${line}-${i}
+  #        echo sed \"${changeLine}d\" $newFile
+               sed  "${changeLine}d"  $newFile > /tmp/sed.$$.txt
+  #        echo mv /tmp/sed.$$.txt $newFile
+               mv /tmp/sed.$$.txt $newFile
+          let i=$i+1
+     done
+  
+     # Add inline closure to any remaining shares
+     for line in `cat $newFile | grep -n "<xnat:share" | cut -d: -f1`
+     do
+  #      echo "sed \"${line}s/>/\/>/\" $newFile"
+             sed "${line}s/>/\/>/" $newFile > /tmp/sed.$$.txt
+        mv /tmp/sed.$$.txt $newFile
+     done   
+  
+     # If there are no more shares in the XML, we need to remove "sharing"
+     areShares=`cat $newFile | grep -n "<xnat:share" | cut -d: -f1`
+     if [ -z "${areShares}" ]; then
+       let i=0 
+       for line in `cat $newFile | grep -n "xnat:sharing" | cut -d: -f1`
+       do
+          let changeLine=${line}-${i}
+          echo "sed \"${changeLine}d\" $newFile"
+                sed  "${changeLine}d" $newFile > /tmp/sed.$$.txt
+          mv /tmp/sed.$$.txt $newFile
+          let i=$i+1
+       done
+     fi
+  
+     # Find/replace strings from replace list 
+     #while read findReplaceStr
+     #do
+     #findStr=`echo $findReplaceStr | cut -d',' -f1`
+     #replaceStr=`echo $findReplaceStr | cut -d',' -f2`
+     #echo "find\/replace $findStr $replaceStr"
+     #if [ ! -z "${findStr}" ]; then
+     #   for line in `cat $newFile | grep -n "${findStr}" | cut -d: -f1`
+     #   do
+     #      echo "sed -i \"${line}s/${findStr}/${replaceStr}/g\" $newFile"
+     #      sed -i "${line}s/${findStr}/${replaceStr}/g" $newFile
+     #   done
+     #fi
+     #done < ${REPLACELIST}   
+   
+  done 
+done
