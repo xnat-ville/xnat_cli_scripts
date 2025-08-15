d799e80ad0d23f560423e8440a1531a66f0d167d
Steve Moore <moore.stephen.m@wustl.edu>
2025-08-15 15:31:32 -0500

    Simplify get_active_project_xml.sh to only retrieve from XNAT. Remove the processing step that is better done elsewhere.

--- scripts/get_active_project_xml.sh
+++ scripts/get_active_project_xml.sh
@@ -35,8 +35,3 @@ BOILER_PLATE=" -a $auth_string -x $url -e False "
 
 rm -rf test_data/active_project_xml test_data/processed_active_project_xml
 get_project_xml_files "$BASE_FOLDER" "$BOILER_PLATE --csv test_data/active_projects.txt --verbose " test_data/active_project_xml
-
-
-echo $BASE_FOLDER/processProjectXML.sh	\
-	test_data/active_project_xml test_data/processed_active_project_xml	\
-	NA test_data/datatypes_to_remove.txt
