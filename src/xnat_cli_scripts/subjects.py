9a12d7e915b76a4ee298664a2e0eab39d638c005
Steve Moore <moore.stephen.m@wustl.edu>
2025-12-15 16:42:09 -0600

    Add function to subjects module to compare two lists of subjects.

--- src/xnat_cli_scripts/subjects.py
+++ src/xnat_cli_scripts/subjects.py
@@ -70,12 +70,51 @@ def execute_list_master(connection: XNATSession, args: argparse.Namespace) -> No
         raise Exception("subjects --list requires --projects")
 
 
+def execute_are_present(connection: XNATSession, args: argparse.Namespace) -> None:
+    test_subjects = create_subject_dictionary(args.test_subjects)
+    reference_subjects = create_subject_dictionary(args.reference_subjects)
+    reference_keys = reference_subjects.keys()
+    count_missing = 0
+    count_not_equal = 0
+    total_values=len(reference_keys)
+    missing_or_errant = []
 
+    for k in reference_keys:
+        reference_value = reference_subjects[k]
+        if k in test_subjects and test_subjects[k] == reference_value:
+            # All good
+            pass
+        elif k in test_subjects:
+            print(f"Test value {test_subjects[k]} differs from {reference_subjects[k]} for {k}")
+            missing_or_errant.append(k)
+            count_not_equal += 1
+        else:
+            print(f"No test value present for {k}")
+            missing_or_errant.append(k)
+            count_missing += 1
+
+    print(f"Missing {count_missing}, Not Equal {count_not_equal}, Total Keys {total_values}")
+    if (args.csv_file):
+        with open(args.csv_file, "w") as csv_output:
+            for subject in missing_or_errant:
+                print(subject, file=csv_output)
+
+def create_subject_dictionary(file_path: str) -> dict:
+    d = dict()
+    with open(file_path, "r") as f:
+        for subject_line in f:
+            tokens = subject_line.split()
+            subject_key = f"{tokens[0]}\t{tokens[1]}"
+            subject_label = tokens[2]
+            d[subject_key] = subject_label
+
+    return d
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="List projects from an XNAT system")
+    parser.add_argument(      '--no_xnat',         dest='no_xnat',                  help='Do not make a connection to an XNAT',        action='store_true' )
     parser.add_argument('-x', '--xnat',            dest='url',                      help="URL to XNAT, default is https://cnda.wustl.edu")
-    parser.add_argument('-a', '--auth',            dest='auth',                     help="User authentication/login for access to XNAT", required=True)
+    parser.add_argument('-a', '--auth',            dest='auth',                     help="User authentication/login for access to XNAT")
     parser.add_argument('-p', '--password',        dest='password',                 help="Password for XNAT authentication", required=False)
     parser.add_argument('-e', '--extension_types', dest='extension_types',          help="True or False for extension_types in xnat.connect")
 
@@ -84,10 +123,13 @@ if __name__ == "__main__":
     parser.add_argument('-R', '--remove',          dest='remove',                   help='Remove groups from projects',                action='store_true')
     parser.add_argument(        '--update',        dest='update',                   help='Update project accessibilities',             action='store_true')
     parser.add_argument(        '--get',           dest='get',                      help='Get a certain type of object at Project Level', action='store_true')
+    parser.add_argument(        '--are_present',   dest='are_present',              help="Test to see if subjects are present",        action='store_true')
 
     # These are objects of the operations;
-    parser.add_argument(       '--projects',        dest='projects',                 help='Include project in output',                  action='store_true')
-    parser.add_argument(       '--session_json',   dest='session_json',             help="Retrieve session JSONs by session ID",       action='store_true')
+    parser.add_argument(       '--projects',        dest='projects',                 help='Include project in output',                 action='store_true')
+    parser.add_argument(       '--test_subjects',   dest='test_subjects',            help="File with list of subjects to be tested")
+    parser.add_argument(       '--reference_subjects', dest='reference_subjects',    help="File with list of reference subjects")
+
 
     ## Further modifiers
     parser.add_argument('-b', '--brief',           dest='brief_format',             help="List in brief format",                       action='store_true')
@@ -99,21 +141,27 @@ if __name__ == "__main__":
     parser.add_argument(      '--template',        dest='template',                 help='Path to a template file')
     args = parser.parse_args()
 
-    args.url = "localhost:8080" if args.url is None else args.url
+    if (args.no_xnat):
+        session = None
+    else:
+        args.url = "localhost:8080" if args.url is None else args.url
 
-    auth_user = xnat_cli_scripts.cli_common.extract_auth_user(args)
-    auth_password = xnat_cli_scripts.cli_common.extract_auth_password(args)
-    xnat_extensions = xnat_cli_scripts.cli_common.extract_extension_types(args)
+        auth_user = xnat_cli_scripts.cli_common.extract_auth_user(args)
+        auth_password = xnat_cli_scripts.cli_common.extract_auth_password(args)
+        xnat_extensions = xnat_cli_scripts.cli_common.extract_extension_types(args)
 
-    session = xnat.connect(args.url, user=auth_user, password=auth_password, extension_types=xnat_extensions)
+        session = xnat.connect(args.url, user=auth_user, password=auth_password, extension_types=xnat_extensions)
 
 try:
     if args.list:
         execute_list_master(session, args)
+    elif args.are_present:
+        execute_are_present(session, args)
     else:
         print("[ERROR] No valid action specified. Use -L, -R, --update, or --get.")
 finally:
-    session.disconnect()  # Ensures cleanup even if an error occurs
+    if session:
+        session.disconnect()  # Ensures cleanup even if an error occurs
 
 
 
